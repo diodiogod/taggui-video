2c65071c113595955bb2cc6fa9adaa68fbe6e33e
diodiogod <160325160+diodiogod@users.noreply.github.com>
2025-11-20 21:39:51 -0300

    Add spell checking to tag mode and autocaption fields
    - Enable spell check red underlines in tag edit boxes
    - Add spell checking to Prompt and Tags to exclude fields
    - Note: Single-line fields (QLineEdit) don't support spell highlighting

--- taggui/utils/text_edit_item_delegate.py
+++ taggui/utils/text_edit_item_delegate.py
@@ -1,6 +1,9 @@
 from PySide6.QtCore import QEvent, QItemSelectionModel, Qt
 from PySide6.QtWidgets import QFrame, QPlainTextEdit, QStyledItemDelegate, QStyle
 
+from utils.spell_highlighter import SpellHighlighter
+from utils.settings import settings
+
 
 class TextEditItemDelegate(QStyledItemDelegate):
     def __init__(self, parent=None):
@@ -20,6 +23,21 @@ class TextEditItemDelegate(QStyledItemDelegate):
             Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         editor.setStyleSheet('padding-left: 3px;')
         editor.index = index
+
+        # Add spell checking if enabled
+        spell_check_enabled = settings.value('spell_check_enabled', defaultValue=True, type=bool)
+        spell_highlighter = SpellHighlighter(editor.document())
+        spell_highlighter.set_enabled(spell_check_enabled)
+
+        # Load custom dictionary
+        custom_dict_list = settings.value('spell_check_custom_dictionary', [], type=list)
+        if custom_dict_list:
+            custom_dict = set(custom_dict_list)
+            spell_highlighter.load_custom_dictionary(custom_dict)
+
+        # Store highlighter reference to prevent garbage collection
+        editor._spell_highlighter = spell_highlighter
+
         return editor
 
     def sizeHint(self, option, index):
